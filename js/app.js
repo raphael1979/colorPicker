765f9cc96dc3f77218b7bc7e947e0fd382347b0d
raphael <moudens.raphael@orange.fr>
2017-03-03 11:24:21 +0100

    ajout fonction .click pour color

--- js/app.js
+++ js/app.js
@@ -1,29 +1,47 @@
 $(document).ready(function(){
 	console.log('coucou');
-	var red=100;
-	var green=100;
-	var blue=100;
+	var red=125;
+	var green=125;
+	var blue=125;
 
-	var Red=100;
-	var Green=100;
-	var Blue=100;
+	var Red=125;
+	var Green=125;
+	var Blue=125;
+	$("#red").click(function(){
+		$(this).on('change mousemove', function(){
+			red = $(this).val();
+			$('body').css({'background-color':"rgb(" + red + "," + green + "," + blue + ")"});
+		});	
+	})
+	$('#green').click(function(){
+		$(this).on('change mousemove', function(){
+			green = $(this).val();
+			$('body').css({'background-color':"rgb(" + red + "," + green + "," + blue + ")"});
+		});
+	})
+	$('#blue').click(function(){
+		$(this).on('change mousemove', function(){
+			blue = $(this).val();
+			$('body').css({'background-color':"rgb(" + red + "," + green + "," + blue + ")"});
+		});
+	})
+	$('#Green').click(function(){
+		$(this).on("change mousemove",function(){
+			Green = $(this).val();
+			$('body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "), rgb(" + red + "," + green + "," + blue + ")"});
+		});
+	})
+	$('#Blue').click(function(){
+		$(this).on("change mousemove",function(){
+			Blue = $(this).val();
+			$('body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "), rgb(" + red + "," + green + "," + blue + ")"});	
+		});
+	})
+	$('#Red').click(function(){
+		$(this).on("change mousemove",function(){
+			Red = $(this).val();
+			$('body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "), rgb(" + red + "," + green + "," + blue + ")"});
 
-	$('#red').on('change mousemove', function(){
-		red = $(this).val();
-		$('body').css({'background-color':"rgb(" + red + "," + green + "," + blue + ")"});
-	});
-	$('#green').on('change mousemove', function(){
-		green = $(this).val();
-		$('body').css({'background-color':"rgb(" + red + "," + green + "," + blue + ")"});
-	});
-	$('#blue').on('change mousemove', function(){
-		blue = $(this).val();
-		$('body').css({'background-color':"rgb(" + red + "," + green + "," + blue + ")"});
-	});
-
-	$('#Green').on("change mousemove",function(){
-		Green = $(this).val();
-		$('body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "), rgb(" + red + "," + green + "," + blue + ")"});
-		console.log($(this).val());
+		});
 	})
 });
