ffe14f41c36375864c494a0b087c6bdc39d8bcc7
raphael <moudens.raphael@orange.fr>
2017-03-03 16:31:28 +0100

    mise en plce gulp minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp =require('gulp');
+var uglify = require('gulp-uglify')
+var minifyInline = require('gulp-minify-inline');
 
 gulp.task('hello', function(){
 	console.log('Hello Zell');
 })
 
-var plugins = require('gulp-load-pluggins')();
+var plugins = require('gulp-load-plugins')();
 
-var source = './js/app.js';
+var source = './js';
 
-var destination = './dist';
+var destination = './dist/';
 
-gulp.task('minify', funtion(){
-	return gulp.src(destination +'' )
-	
-})
+gulp.task('uglify', function(){
+	gulp.src(source +'/app.js')
+	.pipe(uglify())
+	.pipe(gulp.dest(destination));
+
+
+});
